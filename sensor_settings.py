45da4c6e74ff0149e92743ba7d5fc77aff3e8384
EcoSwell <113472300+EcoSwell@users.noreply.github.com>
2023-06-20 18:29:29 -0500

    Update sensor_settings.py
    Signed-off-by: EcoSwell <113472300+EcoSwell@users.noreply.github.com>

--- sensor_settings.py
+++ sensor_settings.py
@@ -27,7 +27,7 @@ Modify the values inside the brackets below for each active sensor with the foll
 i.e. to measure temperature and ammonia with a time delay between readings of 60 and 120 secs 
 and a data recording duration of 4 hours and 7 hours respectively, you should write [(1,60,240), (7,120,420)]
 '''
-sensors = [(1,10,1), (2,10,1), (3,10,1), (4,10,1), (5,10,1), (6,10,1), (7,10,1), (8,10,1)]
+sensors = [(1,25,2), (2,25,2), (3,25,2), (4,25,2), (5,25,2), (6,25,2), (7,25,2), (8,25,2)]
 
 
 
