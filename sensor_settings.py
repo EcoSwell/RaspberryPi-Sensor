0f4e3ce8ca968c5b3a39ad931a1ffcd6ea4f3319
EcoSwell <113472300+EcoSwell@users.noreply.github.com>
2024-11-14 18:45:57 -0500

    Update sensor_settings.py
    Signed-off-by: EcoSwell <113472300+EcoSwell@users.noreply.github.com>

--- sensor_settings.py
+++ sensor_settings.py
@@ -5,7 +5,7 @@ EMAIL ADDRESS
 
 Enter the email address to which the files storing the data readings should be sent:
 '''
-email_address = 'riccardobianco2001@gmail.com'
+email_address = 'hugo.fernandez@ecoswell.org'
 
 
 '''
@@ -27,7 +27,7 @@ Modify the values inside the brackets below for each active sensor with the foll
 i.e. to measure temperature and ammonia with a time delay between readings of 60 and 120 secs 
 and a data recording duration of 4 hours and 7 hours respectively, you should write [(1,60,240), (7,120,420)]
 '''
-sensors = [(1,60,70), (2,60,70), (2,60,70), (3,70,70), (4,60,70), (5,60,70), (6,60,70), (7,60,70), (8,60,70)]
+sensors = [(1,60,3), (2,60,3), (2,60,3), (3,60,3), (4,60,3), (5,60,3), (6,60,3), (7,60,3), (8,60,3)]
 
 
 
